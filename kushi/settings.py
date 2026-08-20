73ea289fc7e70b959a351d595eb138a9cc5d3c5d
TALVI <talvinders583@gmail.com>
2026-08-20 14:48:58 +0530

    Update settings.py

--- kushi/settings.py
+++ kushi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_f)ih^h&(_m(agq^4g_0gx)sf^@=_t4-6xgchw%=^hxx3h=y9b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
